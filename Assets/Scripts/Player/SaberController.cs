c027a01771bae63e9782abd3b8c4f0aaac7c631e
Will1400 <will1400@edu.campusvejle.dk>
2020-05-26 14:58:30 +0200

    Added Basic notes

---
+++ Assets/Scripts/Player/SaberController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaberController : MonoBehaviour
+{
+    public LayerMask layer;
+    public Vector3 previousPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1, layer))
+        {
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+
+        previousPos = transform.position;
+    }
+}
