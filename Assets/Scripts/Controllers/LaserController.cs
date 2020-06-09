aeaa294577f4fb1d13a52ffc5edc6d9b485d0ac4
Will1400 <will1400@edu.campusvejle.dk>
2020-06-09 14:58:59 +0200

    Added obstacle shader

---
+++ Assets/Scripts/Controllers/LaserController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteAlways]
+public class LaserController : MonoBehaviour
+{
+    [SerializeField]
+    Material BlueMaterial;
+    [SerializeField]
+    Material RedMaterial;
+
+    [SerializeField]
+    LineRenderer lineRenderer;
+
+    [SerializeField]
+    float laserLength = 100;
+
+    private void Start()
+    {
+        if (lineRenderer == null)
+            TryGetComponent(out lineRenderer);
+
+
+        lineRenderer.positionCount = 2;
+    }
+
+    void Update()
+    {
+        UpdatePositions();
+    }
+
+
+    void UpdatePositions()
+    {
+
+        lineRenderer.SetPosition(0, transform.TransformPoint(transform.up * laserLength));
+        lineRenderer.SetPosition(1, transform.TransformPoint(-transform.up * laserLength));
+    }
+}
