a063b77c0bf56e4cd54d146da0c488cbff4116a4
Will1400 <Will1400@edu.campusvejle.dk>
2020-06-11 22:42:11 +0200

    Info about the event types

--- Assets/Scripts/SongData/BeatMapData/EventData.cs
+++ Assets/Scripts/SongData/BeatMapData/EventData.cs
@@ -12,6 +12,19 @@ public struct EventData
 
     [JsonProperty("_type")]
     public int Type { get; set; }
+    /* 
+     0 = Back Top Lasers
+     1 = Track Ring Neons
+     2 = Left Lasers
+     3 = Right Lasers
+     4 = Bottom/Back/side Lasers
+     
+     8 = Ring Rotation
+     9 = Small Ring Zoom
+
+    12 = Left Laser Speed
+    13 = Right Laser Speed
+    */
 
     [JsonProperty("_value")]
     public int Value { get; set; }
