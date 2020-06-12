7a0f71d94b9a34e1fea2005baa13c00455931f74
Will1400 <Will1400@edu.campusvejle.dk>
2020-06-12 22:26:25 +0200

    Started adding support for light events

---
+++ Assets/Scripts/Controllers/MasterLaserController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using System.Linq;
+
+public class MasterLaserController : MonoBehaviour
+{
+    [SerializeField]
+    List<LaserController> laserControllers;
+
+    [SerializeField]
+    int supportedEventType; 
+
+    [SerializeField]
+    Material blueMaterial;
+    [SerializeField]
+    Material redMaterial;
+
+    private void Start()
+    {
+        if (laserControllers == null)
+            laserControllers = new List<LaserController>();
+
+        blueMaterial = new Material(blueMaterial);
+        redMaterial = new Material(redMaterial);
+
+        World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EventPlayingSystem>().OnPlayEvent += PlayEvent;
+    }
+
+    private void PlayEvent(int type, int value)
+    {
+        if (type == supportedEventType)
+        {
+            if (value > 4)
+            {
+                value -= 4;
+                laserControllers.ForEach(x => x.SetMaterial(blueMaterial));
+            }
+            else
+            {
+                laserControllers.ForEach(x => x.SetMaterial(redMaterial));
+            }
+
+            switch (value)
+            {
+                case 0:
+                    laserControllers.ForEach(x => x.TurnOff());
+                    break;
+                case 1:
+                    laserControllers.ForEach(x => x.TurnOn());
+                    break;
+                case 2:
+                    laserControllers.ForEach(x => x.Flash());
+                    break;
+                case 3:
+                    laserControllers.ForEach(x => x.Fade());
+                    break;
+                case 13:
+                    // Rotate
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ECS/Systems/Spawning/EventPlayingSystem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using Unity.Collections;
+using System;
+
+public class EventPlayingSystem : SystemBase
+{
+    /// <summary>
+    /// Value 1 is Event Type.
+    /// Value 2 is Event Value.
+    /// </summary>
+    public event Action<int, int> OnPlayEvent;
+
+    public NativeList<EventData> eventsToPlay;
+
+    // Used to always run the system
+    EntityQuery defaultQuery;
+
+    protected override void OnCreate()
+    {
+        eventsToPlay = new NativeList<EventData>(Allocator.Persistent);
+        defaultQuery = GetEntityQuery(new EntityQueryDesc { All = new ComponentType[] { typeof(Entity) } });
+    }
+
+    protected override void OnDestroy()
+    {
+        eventsToPlay.Dispose();
+    }
+
+    protected override void OnUpdate()
+    {
+        if (GameManager.Instance.IsPlaying)
+        {
+            SpawnNeededEvents();
+        }
+    }
+
+    void SpawnNeededEvents()
+    {
+        if (eventsToPlay.IsCreated == false)
+            return;
+
+        for (int i = 0; i < eventsToPlay.Length; i++)
+        {
+            var eventInfo = eventsToPlay[i];
+
+            if (eventInfo.Time - CurrentSongDataManager.Instance.SongSpawningInfo.HalfJumpDuration <= GameManager.Instance.CurrentSongTime)
+            {
+                PlayEvent(eventInfo);
+
+                eventsToPlay.RemoveAtSwapBack(i);
+                i--;
+            }
+        }
+    }
+
+    private void PlayEvent(EventData eventInfo)
+    {
+        OnPlayEvent?.Invoke(eventInfo.Type, eventInfo.Value);
+    }
+}
