4e0ad311366b07e73605fd8615f4989532a38da9
Will1400 <44838295+Will1400@users.noreply.github.com>
2020-08-25 19:06:57 +0200

    Added Buttons to control scroll rect

---
+++ Assets/Scripts/UI/ScrollBarExtensions/ScrollElementsContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace BeatGame.UI.ScrollExtensions
+{
+    public class ScrollElementsContainer : MonoBehaviour
+    {
+        public Action OnContainerChildrenChanged;
+
+        private void OnTransformChildrenChanged()
+        {
+            if (OnContainerChildrenChanged != null)
+            {
+                OnContainerChildrenChanged.Invoke();
+            }
+        }
+    }
+}
--- Assets/Scripts/Utility/Math/MathHelper.cs
+++ Assets/Scripts/Utility/Math/MathHelper.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace BeatGame.Utility.Math
 {
-    public class MathHelper
+    public static class MathHelper
     {
         public static float Map(float value, float fromMin, float fromMax, float toMin, float toMax)
         {
@@ -14,6 +15,12 @@ namespace BeatGame.Utility.Math
             var c = toMin - (m * fromMin);
 
             return m * value + c;
+        }
+
+        public static float RoundDecimalPlaces(this float value, int decimalPlaces)
+        {
+            float multiplier = Mathf.Pow(10f, decimalPlaces);
+            return Mathf.Round(value * multiplier) / multiplier;
         }
     }
 }
