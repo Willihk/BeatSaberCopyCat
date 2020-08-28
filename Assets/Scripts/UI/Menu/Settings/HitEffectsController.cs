158e19639d551a3e1efd9902d2305eedfeea6306
Will1400 <will1400@edu.campusvejle.dk>
2020-08-28 10:36:23 +0200

    Fixed hit effect setting

---
+++ Assets/Scripts/UI/Menu/Settings/HitEffectsController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using BeatGame.Logic.Managers;
+
+namespace BeatGame.UI.Controllers
+{
+    public class HitEffectsController : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject checkMark;
+
+        private void OnEnable()
+        {
+            if (SettingsManager.Instance.Settings["General"]["HitEffects"].IntValue == 0)
+            {
+                checkMark.SetActive(false);
+            }
+        }
+
+        public void Toggle()
+        {
+            if (SettingsManager.Instance.Settings["General"]["HitEffects"].IntValue == 0)
+            {
+                SettingsManager.Instance.UpdateConfigSetting("General", "HitEffects", 1);
+                checkMark.SetActive(true);
+            }
+            else
+            {
+                SettingsManager.Instance.UpdateConfigSetting("General", "HitEffects", 0);
+                checkMark.SetActive(false);
+            }
+        }
+    }
+}
