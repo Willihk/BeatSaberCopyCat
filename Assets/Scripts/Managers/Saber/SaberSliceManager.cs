c65f6ded418ca88fbbad7678f1cbe05630953174
Will1400 <will1400@edu.campusvejle.dk>
2020-09-16 10:33:26 +0200

    Added force to sliced objects

--- Assets/Scripts/Managers/Saber/SaberSliceManager.cs
+++ Assets/Scripts/Managers/Saber/SaberSliceManager.cs
@@ -26,12 +26,21 @@ namespace BeatGame.Logic.Managers
             baseNote.transform.position = noteTransform.position;
             baseNote.transform.rotation = noteTransform.rotation;
 
-            SlicedHull hull = baseNote.Slice(baseNote.gameObject.transform.position, direction, LeftSaberMaterial);
+            Material material = noteType == 1 ? RightSaberMaterial : LeftSaberMaterial;
+
+            SlicedHull hull = baseNote.Slice(baseNote.gameObject.transform.position, direction, material);
 
             if (hull != null)
             {
-                hull.CreateLowerHull(baseNote.gameObject, LeftSaberMaterial);
-                hull.CreateUpperHull(baseNote.gameObject, LeftSaberMaterial);
+                GameObject hullObject = hull.CreateLowerHull(baseNote.gameObject, LeftSaberMaterial);
+                var rigidbody = hullObject.AddComponent<Rigidbody>();
+                rigidbody.AddExplosionForce(2, noteTransform.position, .5f);
+                Destroy(hullObject, 10);
+
+                hullObject = hull.CreateUpperHull(baseNote.gameObject, LeftSaberMaterial);
+                rigidbody = hullObject.AddComponent<Rigidbody>();
+                rigidbody.AddExplosionForce(2, noteTransform.position, .5f);
+                Destroy(hullObject, 10);
             }
         }
     }
