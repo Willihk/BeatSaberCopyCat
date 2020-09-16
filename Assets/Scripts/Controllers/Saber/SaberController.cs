60d2a3bb86e798ec48bb258dd43f6b89edb75955
Will1400 <will1400@edu.campusvejle.dk>
2020-09-16 10:17:39 +0200

    Testing with note slicing

--- Assets/Scripts/Controllers/Saber/SaberController.cs
+++ Assets/Scripts/Controllers/Saber/SaberController.cs
@@ -183,7 +183,46 @@ namespace BeatGame.Logic.Saber
 
         private void DestroyNote(Entity entity)
         {
+            // Slice Mesh
+
+            ThreePointsToBox(tipPoint.position, basePoint.position, previousTipPosition + previousBasePosition * 0.5f, out Vector3 center, out Vector3 halfSize, out Quaternion orientation);
+
+            Vector3 direction = orientation * Vector3.up;
+
+            SaberSliceManager.Instance.Slice(fakeNoteTransform, direction,EntityManager.GetComponentData<Note>(entity).Type);
+
             EntityManager.DestroyEntity(entity);
+        }
+
+
+        public void ThreePointsToBox(Vector3 p0, Vector3 p1, Vector3 p2, out Vector3 center, out Vector3 halfSize, out Quaternion orientation) //https://github.com/hrincarp/GGJ2017-cart/
+        {
+            Vector3 up = Vector3.Cross(p1 - p2, p0 - p2).normalized;
+
+            // Continue only if normal exists
+            if (up.sqrMagnitude > 0.00001f)
+            {
+
+                Vector3 forward = (p0 - p1).normalized;
+                Vector3 left = Vector3.Cross(forward, up);
+
+                orientation = new Quaternion();
+                orientation.SetLookRotation(forward, up);
+
+                float a = Mathf.Abs((new UnityEngine.Plane(left, p0)).GetDistanceToPoint(p2));
+                float b = Vector3.Magnitude(p0 - p1);
+
+                Vector3 pc = (p0 + p1) * 0.5f;
+
+                center = pc - left * a * 0.5f;
+                halfSize = new Vector3(a * 0.5f, 0.0f, b * 0.5f);
+            }
+            else
+            {
+                center = Vector3.zero;
+                halfSize = Vector3.zero;
+                orientation = Quaternion.identity;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Managers/Saber/SaberSliceManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using BeatGame.Data.Map.Modified;
+using EzySlice;
+
+namespace BeatGame.Logic.Managers
+{
+    public class SaberSliceManager : MonoBehaviour
+    {
+        public static SaberSliceManager Instance;
+
+        public GameObject baseNote;
+
+        public Material LeftSaberMaterial;
+        public Material RightSaberMaterial;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+
+        public void Slice(Transform noteTransform, Vector3 direction, int noteType)
+        {
+            baseNote.transform.position = noteTransform.position;
+            baseNote.transform.rotation = noteTransform.rotation;
+
+            SlicedHull hull = baseNote.Slice(baseNote.gameObject.transform.position, direction, LeftSaberMaterial);
+
+            if (hull != null)
+            {
+                hull.CreateLowerHull(baseNote.gameObject, LeftSaberMaterial);
+                hull.CreateUpperHull(baseNote.gameObject, LeftSaberMaterial);
+            }
+        }
+    }
+}
