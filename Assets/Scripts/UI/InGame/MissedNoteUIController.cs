a93211b518c50c5d4365f87d20d8789c81e0c6af
Will1400 <44838295+Will1400@users.noreply.github.com>
2020-10-04 21:27:30 +0200

    Added logic for missed note ui

---
+++ Assets/Scripts/UI/InGame/MissedNoteUIController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using BeatGame.Logic.Managers;
+using Unity.Mathematics;
+
+namespace BeatGame.UI.Controllers
+{
+    public class MissedNoteUIController : MonoBehaviour
+    {
+        [SerializeField]
+        int noteType;
+        [SerializeField]
+        Image missImage;
+
+        [SerializeField]
+        Vector3 startPosition;
+        [SerializeField]
+        Vector3 endPosition;
+
+        [SerializeField]
+        Color startColor = new Color(1, .5f, .5f, .5f);
+        [SerializeField]
+        Color endColor = new Color(1, 1, 1, 1);
+
+        [SerializeField]
+        AnimationCurve movementCurve;
+
+        private void OnEnable()
+        {
+            missImage.enabled = false;
+            if (GameEventManager.Instance != null)
+                GameEventManager.Instance.OnNoteMissed += MissedNote;
+        }
+
+        private void OnDisable()
+        {
+            if (GameEventManager.Instance != null)
+                GameEventManager.Instance.OnNoteMissed -= MissedNote;
+        }
+
+        void MissedNote(int type)
+        {
+            if (type == noteType)
+            {
+                StopAllCoroutines();
+                StartCoroutine(AnimateMissImage(.5f));
+            }
+        }
+
+        IEnumerator AnimateMissImage(float duration)
+        {
+            missImage.enabled = true;
+            missImage.rectTransform.localPosition = startPosition;
+
+            float startTime = Time.time;
+
+            while (true)
+            {
+                float elapsed = Time.time - startTime;
+
+                missImage.rectTransform.localPosition = Vector3.Lerp(startPosition, endPosition, movementCurve.Evaluate(elapsed / duration));
+                missImage.color = Color.Lerp(startColor, endColor, movementCurve.Evaluate(elapsed / duration));
+                if (elapsed >= duration)
+                    break;
+
+                yield return null;
+            }
+
+            startTime = Time.time;
+
+            while (true)
+            {
+                float elapsed = Time.time - startTime;
+                Color color = endColor;
+                color.a = 0;
+                missImage.color = Color.Lerp(endColor, color, movementCurve.Evaluate(elapsed / duration));
+
+                if (elapsed >= duration / 2)
+                    break;
+
+                yield return null;
+            }
+            missImage.enabled = false;
+        }
+    }
+}
