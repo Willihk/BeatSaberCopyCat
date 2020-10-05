d40c81fb3fa3f605c1c8b1bb00820102392da0b5
Will1400 <will1400@edu.campusvejle.dk>
2020-10-05 14:02:51 +0200

    Added pointer controller switching

---
+++ Assets/Scripts/Controllers/VR/PointerControllerSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+namespace Assets.Scripts.Controllers.VR
+{
+    public class PointerControllerSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        Transform leftController;
+        [SerializeField]
+        Transform rightController;
+
+        [SerializeField]
+        Transform pointer;
+
+        [SerializeField]
+        SteamVR_Action_Boolean interactWithUIAction;
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (interactWithUIAction == null)
+                return;
+
+            if (interactWithUIAction.GetStateDown(SteamVR_Input_Sources.RightHand))
+            {
+                pointer.SetParent(rightController);
+                pointer.transform.localPosition = Vector3.zero;
+                pointer.transform.localRotation = Quaternion.identity;
+            }
+            else if (interactWithUIAction.GetStateDown(SteamVR_Input_Sources.RightHand))
+            {
+                pointer.SetParent(leftController);
+                pointer.transform.localPosition = Vector3.zero;
+                pointer.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+}
